fe1f68b9e3787db596dc60b77ac91b4bb3831081
Akshay Honap <akshayhonap619@gmail.com>
2018-04-27 17:29:38 -0400

    Redux started

--- upgrade-build/reducers/book-reducer.js
+++
@@ -1,9 +0,0 @@
-export function bookreducer(){
-    return [
-        {title: "Harry Potter"},
-        {title: "The Dark Knight"},
-        {title: "Winter Is Here"},
-        {title: "If you smell, What the Rock is cooking"},
-    ]
-
-}
