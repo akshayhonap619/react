07ca26806ae212d085240d471468304fd3b984b2
Akshay Honap <akshayhonap619@gmail.com>
2018-05-08 14:56:09 -0400

    Create Component using connect

--- upgrade-build/components/Expensify-App/Filters.js
+++ upgrade-build/components/Expensify-App/Filters.js
@@ -3,28 +3,32 @@ import ReactDOM from 'react-dom'
 
 import {connect} from 'react-redux'
 
-class Filters extends React.Component{
-    constructor(props){
-        super(props)
+// class Filters extends React.Component{
+//     constructor(props){
+//         super(props)
+//
+//     }
+//
+//     changeText(event){
+//         this.setState({
+//             text : event.target.value
+//         })
+//     }
+//
+//     render(){
+//         return(
+//             <div>
+//                 <input type="text" value={props.filters.text} onChange={this.changeText}/>
+//             </div>
+//
+//         )
+//     }
+// }
 
-    }
 
-    changeText(event){
-        this.setState({
-            text : event.target.value
-        })
-    }
-
-    render(){
-        return(
-            <div>
-                <input type="text" value={props.filters.text} onChange={this.changeText}/>
-            </div>
-
-        )
-    }
-}
-
+const Filters = (props)=>(
+    <input type="text" value={props.filters.text}/>
+)
 
 const mapStateToProps = (state)=>({
     filters : state.filters
--- upgrade-build/redux/Expensify-App.js
+++ upgrade-build/redux/Expensify-App.js
@@ -7,10 +7,13 @@ import {Provider} from 'react-redux'
 import {store} from '../redux/store/configureStore'
 
 import {AddExpense,updateExpense,deleteExpense} from "./actions/expense-actions";
+import {setTextFilter} from "./actions/filter-actions";
 
 console.log(store.getState())
 
 setTimeout(()=>  store.dispatch(AddExpense({description:'Rent', amount : 500}) ) , 3000)
+
+store.dispatch(setTextFilter("Rent"))
 
 ReactDOM.render(
     <Provider store={store}>
